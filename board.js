777c44e54552a919fc2b38fdab4b6cd8a2a1afb1
NTX0325 <ad0124578963@gmail.com>
2021-12-19 20:54:22 +0800

    up

--- board.js
+++ board.js
@@ -1,3 +1,25 @@
-$( document ).ready(function() {
-    console.log( "ready!" ); 
+var input, yourname;
+$(document).ready(function () {
+    $("#tap").click(function () {
+        input = $("#comment").val();
+        yourname = $("#yourname").val();
+        tap();
+        $("input").val("");
+        $("textarea").val("");
+    });
 });
+
+function tap() {
+    var floor = $("<div class='container mt-3 mes'>");
+    floor.attr("id", "message");
+    floor.append(
+        $("<p class=firstname >").html("姓名: "+yourname),
+        $("<p class=text >").html("內容: " + input),
+        $("<button class='btn btn-outline-danger delete'>").html("刪除").attr("id", "del"),
+        $("<button class='btn btn-outline-success edt'>").html("編輯").attr("id", "edit")
+    );
+    $("#messages").append(floor);
+    $("#messages").on("click", "#del", function () {
+        $(this).parents("#message").remove();
+    });
+} 
